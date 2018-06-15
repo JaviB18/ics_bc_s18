86286061fc3ebf7fd01cb46e94b96f4bbead792d
JaviB18 <txrodrigues123@gmail.com>
2018-06-14 17:13:29 -0700

    complete author_age exercise

---
+++ week1/ch05/bigger_better_favorite_number.rb
@@ -0,0 +1,11 @@
+puts 'Now that I know your name, let\'s get to know each other!  What\'s your favorite number?'
+pi = gets.chomp
+pi2 = pi.to_f + 1.to_f
+pi2 = pi2.to_s
+puts 'Wow, cool!  My favorite number is ' + pi2
+puts 'So, ya see, if ya simply add 1 to ' + pi.to_s + ', you\'ll get ' + pi2 + '!'
+puts 'Which is bigger!'
+puts 'And since it\'s bigger and my favorite, ' + pi2 + ' makes for a much better number!'
+puts 'Don\'tcha think?'
+pi3 = gets.chomp
+puts 'LOL'
---
+++ week1/ch05/full_name_greeting.rb
@@ -0,0 +1,9 @@
+puts 'Well howdy stranger!  What\'s your name?'
+first = gets.chomp
+puts 'so ' + first + ', eh?  I knew a ' + first + ' once.  What\'s your last name?'
+last = gets.chomp
+puts first + ' ' + last + '...  Not ringin\' any bells.  Ya got a middle name?'
+middle = gets.chomp
+puts first + ' ' + middle + ' ' + last + '?  Well, shucks, looks like I don\'t know ya.'
+puts 'But now I do!'
+puts 'Nice to meet\'cha ' + first + ' ' + middle + ' ' + last + '!'  
